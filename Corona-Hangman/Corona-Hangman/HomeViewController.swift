ba987f60a4da9e7bcd6f7b7d6d87b80406df48ef
Arjun Dureja <arjun.dureja1000@gmail.com>
2020-05-23 17:26:58 -0400

    initial commit

---
+++ Corona-Hangman/Corona-Hangman/HomeViewController.swift
@@ -0,0 +1,36 @@
+//
+//  HomeViewController.swift
+//  Corona-Hangman
+//
+//  Created by Arjun Dureja on 2020-05-23.
+//  Copyright © 2020 Arjun Dureja. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var playBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor(patternImage: UIImage(named: "background")!)
+        navigationController?.navigationBar.isHidden = true
+        titleLabel.font = UIFont(name: "ChalkboardSE-Bold", size: 50)
+        titleLabel.widthAnchor.constraint(equalToConstant: 210).isActive = true
+
+        titleLabel.numberOfLines = 0
+        titleLabel.textColor = .black
+        
+        imageView.image = UIImage(named: "image8")
+        
+        playBtn.layer.cornerRadius = 10
+        playBtn.titleLabel?.font = UIFont(name: "ChalkboardSE-Bold", size: 17)
+    }
+    
+    @IBAction func playClicked(_ sender: Any) {
+        let vc = storyboard?.instantiateViewController(identifier: "game")
+        navigationController?.pushViewController(vc!, animated: true)
+    }
+}
